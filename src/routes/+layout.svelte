d3f6a7e28f0127e38dd831d1516054e2336e9603
YuriLv.100 <202211257@gordoncollege.edu.ph>
2024-10-08 11:45:33 +0800

    install google fonts and taildwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>import "../app.css";</script>
+
+ <svelte:head> 
+
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+ </svelte:head>
+
+
+<slot></slot>
